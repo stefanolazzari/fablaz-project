1c1c851b250b23e6ebd24d8e740e91aca4b89e4c
STEFANO LAZZARI <stefanolazzari79@libero.it>
2023-11-21 06:34:06 +0100

    Clean up dropdown

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -12,7 +12,7 @@
     </div>
 
 
-        <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 z-50" style="display: none">
+        <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 z-50 overflow-auto max-h-52" style="display: none">
             {{ $slot }}
         </div>
         
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,9 +4,9 @@
 
         <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
                 @if ($posts->count())
-                <x-posts-grid :posts="$posts"/>
+                        <x-posts-grid :posts="$posts"/>
                 @else
-                <p class="text-center">No posts yet </p>
+                        <p class="text-center">No posts yet </p>
                 @endif
         </main>
 </x-layout>
